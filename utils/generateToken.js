37a3132cccb1fae27fb5b9c7926bb2c4f7144280
M R <monirujjamanmamun2357@gmail.com>
2025-03-15 13:38:48 +0600

    generateToken file fix jwt_secret issus

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,7 +5,7 @@ const { jwt_secret } = require('./allSecretExpt/allSecretExpt');
 const generateToken = async (user) => {
   return jwt.sign(
     { uid: user.uid, email: user.email, role: user.role }, // Payload
-    process.env.jwt_secret, // Secret key
+    jwt_secret, // Secret key
     { expiresIn: '1d' } // Token expires in 7 days
   );
 };
